d31d8415a392bec8c3a73b177a2606bae0c8473c
AbhijeetArjeet <abhijeetarjeet@users.noreply.github.com>
2026-08-31 16:36:16 +0530

    release(android): v1.0.4 - 100% on-device standalone downloader

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.ypdlp.downloader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 4
+        versionName = "1.0.4"
 
         ndk {
             abiFilters.addAll(setOf("arm64-v8a", "armeabi-v7a", "x86", "x86_64"))
